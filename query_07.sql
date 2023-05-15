ae3bccc7f464c0496b0a08d7a8953fbe6bdaa41c
Gustavo Costa <74384665+Lu1z-Gust4v0@users.noreply.github.com>
2023-05-15 19:53:38 +0000

    Add queries 07 and 08

---
+++ query_07.sql
@@ -0,0 +1,16 @@
+-- Metricas da URA
+
+SELECT motivo, tempo_medio_atendimento, qntd, resolvidos, ROUND(resolvidos / CAST(qntd AS NUMERIC), 4) as taxa_de_resolucao, custo_total FROM (
+  SELECT motivo, ROUND(AVG(tempo_atendimento), 2) AS tempo_medio_atendimento, COUNT(motivo) AS qntd,
+    SUM(
+      CASE 
+				WHEN resolvido = 'Sim' THEN 1
+				ELSE 0
+			END	
+    ) AS resolvidos,
+		ROUND(SUM(CAST(REPLACE(custo, ',', '.') AS NUMERIC)), 2) as custo_total
+	FROM chamados
+	JOIN custos ON chamados.id_chamado = custos.id_chamado 
+  WHERE canal = 'URA'
+	GROUP BY motivo 
+) AS subset ORDER BY qntd DESC;
